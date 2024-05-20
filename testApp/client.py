5153e77e1c44329136b768d884f97d3c43ff625b
Milana Vorobeva <vorobey2317@mail.ru>
2024-05-20 13:24:42 +0300

    fix test app

--- testApp/client.py
+++ testApp/client.py
@@ -1,19 +1,21 @@
 import socket as sct
 
-host = "84.38.181.98"
-port = 23
-pcg_size = 1024
 
-socket = sct.socket(sct.AF_INET, sct.SOCK_STREAM)
-socket.connect((host, port))
+if __name__ == '__main__':
+    host = "84.38.181.98"
+    port = 23
+    pcg_size = 1024
 
-print('start getting cytoscape connection status')
+    socket = sct.socket(sct.AF_INET, sct.SOCK_STREAM)
+    socket.connect((host, port))
 
-status = socket.recv(pcg_size)
-status = int.from_bytes(status, byteorder='big')
-print(f'status {status} was got')
-msg = f'received cytoscape connection status: {status}'
-print(msg)
-socket.send(msg.encode())
+    print('start getting cytoscape connection status')
 
-print('finish getting cytoscape connection status')
+    status = socket.recv(pcg_size)
+    status = int.from_bytes(status, byteorder='big')
+    print(f'status {status} was got')
+    msg = f'received cytoscape connection status: {status}'
+    print(msg)
+    socket.send(msg.encode())
+
+    print('finish getting cytoscape connection status')
--- testApp/server.py
+++ testApp/server.py
@@ -1,21 +1,24 @@
 import socket as sct
 
 
-host = "84.38.181.98"
-port = 23
-pcg_size = 1024
-listeners_amount = 2
+if __name__ == '__main__':
+    host = "84.38.181.98"
+    port = 23
+    pcg_size = 1024
+    listeners_amount = 2
 
-socket = sct.socket(sct.AF_INET, sct.SOCK_STREAM)
-socket.bind((host, port))
-socket.listen(listeners_amount)
+    socket = sct.socket(sct.AF_INET, sct.SOCK_STREAM)
+    socket.bind((host, port))
+    socket.listen(listeners_amount)
 
-print('start sending cytoscape connection status')
-status = 1
-status_bin = status.to_bytes(length=8, byteorder='big')
-socket.send(status_bin+b'\n')
-print(f'status {status} was sent')
-response = socket.recv(pcg_size).decode()
-print(f'client response: {response}')
+    conn, address = socket.accept()
 
-print('finish sending cytoscape connection status')
+    print('start sending cytoscape connection status')
+    status = 1
+    status_bin = status.to_bytes(byteorder='big')
+    conn.send(status_bin)
+    print(f'status {status} was sent')
+    response = conn.recv(pcg_size).decode()
+    print(f'client response: {response}')
+
+    print('finish sending cytoscape connection status')
